3487cc64d4827cb888d5ec2040e758516586f922
Roselma Mendes <roselma.mendes@gmail.com>
2015-08-20 13:56:10 -0300

    Delete some files and folders.

--- app/scripts/commons/filters/start-from.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('punchClockAngular')
-  .filter('startFrom', function () {
-    return function(input, start) {
-      start = +start; //parse to int
-      return (typeof input === 'object' && input.length > 0 ) ? input.slice(start) : [];
-    };
-  });
